34d1962692c0d00a38142c823efcb0c5fa5674ee
Dipanshi Verma <dipanshiverma0@gmail.com>
2025-06-21 11:14:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,68 +1,14 @@
-const app = document.getElementById('app');
-const video = document.getElementById('bg-video');
-const sound = document.getElementById('meditation-sound');
-const playBtn = document.querySelector('.play');
-const timeDisplay = document.querySelector('.time-display');
-const timeButtons = document.querySelectorAll('#time-select button');
-const soundButtons = document.querySelectorAll('.sound-picker button');
-
-let fakeDuration = 600; // default 10 min
-let timer;
-
-// Update time on screen
-function updateTimeDisplay(seconds) {
-  const mins = Math.floor(seconds / 60);
-  const secs = Math.floor(seconds % 60);
-  timeDisplay.textContent = `${mins}:${secs < 10 ? '0' : ''}${secs}`;
-}
-
-// Play / Pause toggle
-playBtn.addEventListener('click', () => {
-  if (sound.paused) {
-    sound.play();
-    video.play();
-    playBtn.textContent = 'Pause';
-
-    timer = setInterval(() => {
-      fakeDuration--;
-      updateTimeDisplay(fakeDuration);
-
-      if (fakeDuration <= 0) {
-        clearInterval(timer);
-        sound.pause();
-        video.pause();
-        sound.currentTime = 0;
-        updateTimeDisplay(fakeDuration = 600);
-        playBtn.textContent = 'Play';
-      }
-    }, 1000);
-  } else {
-    sound.pause();
-    video.pause();
-    clearInterval(timer);
-    playBtn.textContent = 'Play';
-  }
-});
-
-// Change duration
-timeButtons.forEach(button => {
-  button.addEventListener('click', function () {
-    fakeDuration = this.getAttribute('data-time');
-    updateTimeDisplay(fakeDuration);
-  });
-});
-
-// Switch sound & video
 soundButtons.forEach(button => {
-  button.addEventListener('click', function () {
-    sound.src = this.getAttribute('data-sound');
-    video.src = this.getAttribute('data-video');
-    if (!sound.paused) {
+  button.addEventListener("click", function () {
+    const newSound = this.getAttribute("data-sound");
+    const newVideo = this.getAttribute("data-video");
+
+    sound.src = newSound;
+    video.src = newVideo;
+
+    if (isPlaying) {
       sound.play();
       video.play();
     }
   });
 });
-
-// Initial time display
-updateTimeDisplay(fakeDuration);
